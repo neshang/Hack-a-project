6d8cf9aa7eee970345016898139fe0dbb2003cf4
Enyi Shang <enyishang@dyn3154-14.wlan.ic.ac.uk>
2018-03-25 19:40:49 +0100

    speech recognition and print output in textarea

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('page.html')
+    return render_template('index.html')
--- src/static/js/speechRecognition.js
+++ src/static/js/speechRecognition.js
@@ -1,57 +1,94 @@
-$(document).ready(function() {
-  var speech = [];
+var speech = [];
 
-  var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-  recognition.lang = 'en-US';
-  recognition.interimResults = true;
-  recognition.continuous = true;
-  recognition.maxAlternatives = 5;
+var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+recognition.lang = 'en-US';
+recognition.continuous = true;
+recognition.maxAlternatives = 5;
+
+var dicTextarea = $('#dic-textarea');
+var noteContent = '';
+
+/* Buttons and input */
+$('#start-record-btn').on('click', function(e) {
+  if (noteContent.length) {
+    noteContent += ' ';
+  }
   recognition.start();
-
-  recognition.onresult = function(event) {
-    var interim_transcript = '';
-    var final_transcript = '';
-
-    comsole.log(event.results);
-
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
-
-    console.log(interim_transcript);
-    console.log('Final: ' + final_transcript);
-  };
-
-  recognition.onerror = function (event) {
-    console.log(event.error);
-  };
-
-  recognition.onend = function (event) {
-    recognition.start();
-  };
-
-  recognition.onstart = function() {
-    console.log('Speech recognition service has started');
-  }
-
-
-  recognition.onsoundstart = function() {
-    console.log('Some sound is being received');
-  }
-
-  function produceMessage(){
-      var msg= 'Hello<br />';
-      console.log(msg);
-      return msg;
-  }
-
-  function getSpeech(){
-      console.log('return speech array');
-      console.log('the speech array is' + speech);
-      return speech;
-  }
+  console.log("Recognition start...");
 });
+
+$('#pause-record-btn').on('click', function(e) {
+  recognition.stop();
+  console.log("Recognition stop...");  
+});
+
+dicTextarea.on('input', function() {
+  noteContent = $(this).val();
+})
+
+$('#finish-record-btn').on('click', function(e) {
+  recognition.stop();
+  console.log("Finish record button is pressed...");
+  
+
+  // if(!noteContent.length) {
+  //   instructions.text('Could not save empty note. Please add a message to your note.');
+  // }
+  // else {
+  //   // Save note to localStorage.
+  //   // The key is the dateTime with seconds, the value is the content of the note.
+  //   saveNote(new Date().toLocaleString(), noteContent);
+
+  //   // Reset variables and update UI.
+  //   noteContent = '';
+  //   renderNotes(getAllNotes());
+  //   noteTextarea.val('');
+  //   instructions.text('Note saved successfully.');
+  // }
+})
+
+recognition.onresult = function(event) {
+  let final_transcript = '';
+
+  for (let i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+      final_transcript += event.results[i][0].transcript;
+      final_transcript += '. '
+      console.log(`The final transcript is: ${final_transcript}`)
+      dicTextarea.val(dicTextarea.val() + final_transcript)
+    } 
+  }
+}
+
+//   console.log(interim_transcript);
+//   console.log('Final: ' + final_transcript);
+// };
+
+// recognition.onerror = function (event) {
+//   console.log(event.error);
+// };
+
+// recognition.onend = function (event) {
+//   recognition.start();
+// };
+
+// recognition.onstart = function() {
+//   console.log('Speech recognition service has started');
+// }
+
+
+// recognition.onsoundstart = function() {
+//   console.log('Some sound is being received');
+// }
+
+// function produceMessage(){
+//     var msg= 'Hello<br />';
+//     console.log(msg);
+//     return msg;
+// }
+
+// function getSpeech(){
+//     console.log('return speech array');
+//     console.log('the speech array is' + speech);
+//     return speech;
+// }
