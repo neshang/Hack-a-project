029d8d1f24269e42c02b5fe6eaf5f9eb61e30c68
Enyi Shang <enyishang@dyn3154-14.wlan.ic.ac.uk>
2018-03-25 22:40:36 +0100

    print out the word and frequency on the right

--- src/static/js/speechRecognition.js
+++ src/static/js/speechRecognition.js
@@ -1,15 +1,34 @@
-var speech = [];
+var speech = []
 
 var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-recognition.lang = 'en-US';
-recognition.continuous = true;
-recognition.maxAlternatives = 5;
+recognition.lang = 'en-US'
+recognition.continuous = true
+recognition.maxAlternatives = 5
 
-var dicTextarea = $('#dic-textarea');
-var noteContent = '';
+var dicTextarea = $('#dic-textarea')
+var result = $('#analysis-result')
+var noteContent = ''
+
+/* Set of words that are not tracked */
+const untracked_words = new Set(["the","be","to","of","and","a","in","that","have","I","it","for","not","on","with",
+                                "he","as","you","do","at","this","but","his","by","from","they","we","say","her","she",
+                                "or","an","will","my","one","all","would","there","their","what","so","up","out","if",
+                                "about","who","get","which","go","me","when","make","can","like","time","no","just",
+                                "him","know","take","people","into","year","your","good","some","could","them","see",
+                                "other","than","then","now","look","only","come","its","over","think","also","back",
+                                "after","use","two","how","our","work","first","well","way","even","new","want","because",
+                                "any","these","give","day","most","us", "it's","I'll", "off"])
+
+/* A map keeps track of word and frequency */
+var word_freq = new Map()
 
 /* Buttons and input */
 $('#start-record-btn').on('click', function(e) {
+  /* Clean up the textareas */
+  dicTextarea.val("")
+  document.getElementById('analysis-result').innerHTML = ""
+  work_freq = new Map()
+
   if (noteContent.length) {
     noteContent += ' ';
   }
@@ -28,38 +47,50 @@ dicTextarea.on('input', function() {
 
 $('#finish-record-btn').on('click', function(e) {
   recognition.stop();
+  document.getElementById('analysis-result').innerHTML = ""
+  word_freq.forEach(appendResult);
+
   console.log("Finish record button is pressed...");
-  
-
-  // if(!noteContent.length) {
-  //   instructions.text('Could not save empty note. Please add a message to your note.');
-  // }
-  // else {
-  //   // Save note to localStorage.
-  //   // The key is the dateTime with seconds, the value is the content of the note.
-  //   saveNote(new Date().toLocaleString(), noteContent);
-
-  //   // Reset variables and update UI.
-  //   noteContent = '';
-  //   renderNotes(getAllNotes());
-  //   noteTextarea.val('');
-  //   instructions.text('Note saved successfully.');
-  // }
 })
+
+function appendResult(value, key, map) {
+  document.getElementById('analysis-result').innerHTML += `<span class="word"> ${key}</span>`
+  document.getElementById('analysis-result').innerHTML += `<span class="frequency"> ${value}</span>`
+  document.getElementById('analysis-result').innerHTML += `<br>`
+  
+}
 
 recognition.onresult = function(event) {
   let final_transcript = '';
 
   for (let i = event.resultIndex; i < event.results.length; ++i) {
     if (event.results[i].isFinal) {
-      final_transcript += event.results[i][0].transcript;
+      final_transcript += $.trim(event.results[i][0].transcript)
+      analysisSentence(final_transcript)
+      final_transcript = capitalizeFirstLetter(final_transcript)
       final_transcript += '. '
-      console.log(`The final transcript is: ${final_transcript}`)
       dicTextarea.val(dicTextarea.val() + final_transcript)
     } 
   }
 }
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function analysisSentence(string) {
+  var words = string.split(" ")
+  for (var i = 0; i < words.length; i += 1) {
+    if (!untracked_words.has(words[i])) {
+      if (word_freq.has(words[i])) {
+        var freq = word_freq.get(words[i])
+        word_freq.set(words[i], freq++)
+      } else {
+        word_freq.set(words[i], 1)
+      }
+    }
+  }
+}
 //   console.log(interim_transcript);
 //   console.log('Final: ' + final_transcript);
 // };
